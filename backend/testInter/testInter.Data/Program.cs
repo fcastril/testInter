c356a7bcad9a3053b00107f3334947c196d0ba01
Fabián Mauricio Castrillón Franco <fcastril@gmail.com>
2021-03-17 21:47:19 -0500

    Estructura Inicial de Proyectos BACKEND

---
+++ backend/testInter/testInter.Data/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace testInter.Data
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
