a51bf14dd810a006ff8d5f3a0bac82ad64203ead
Fabián Mauricio Castrillón Franco <fcastril@gmail.com>
2021-03-18 21:11:54 -0500

    Create First POCO Entities

---
+++ backend/testInter/testInter.Data/Employee.cs
@@ -0,0 +1,11 @@
+﻿namespace testInter.Data
+{
+    public class Employee
+    {
+        public int idEmployee { get; set; }
+        public string DocumentId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Position { get; set; }
+    }
+}
