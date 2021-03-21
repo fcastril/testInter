7a99fb550b9bd056cb2958c8f08b230a44d659b1
Fabián Mauricio Castrillón Franco <fcastril@gmail.com>
2021-03-21 12:31:40 -0500

    Implementig Swagger

--- backend/testInter/testInter.WebAPI/Startup.cs
+++ backend/testInter/testInter.WebAPI/Startup.cs
@@ -6,6 +6,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using testInter.Repo;
 using testInter.Service;
+using Microsoft.OpenApi.Models;
+using System;
 
 namespace testInter.WebAPI
 {
@@ -26,6 +28,29 @@ namespace testInter.WebAPI
             services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
             services.AddTransient<IUserService, UserService>();
             services.AddTransient<IEmployeeService, EmployeeService>();
+
+
+            AddSwagger(services);
+        }
+
+        private void AddSwagger(IServiceCollection services)
+        {
+            services.AddSwaggerGen(options =>
+            {
+                var groupName = "v1";
+
+                options.SwaggerDoc(groupName, new OpenApiInfo
+                {
+                    Title = $"InterTest {groupName}",
+                    Version = groupName,
+                    Description = "InterTest API",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Fabian Mauricio Castrillón Franco",
+                        Email = "fcastril@gmail.com"
+                    }
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,6 +62,12 @@ namespace testInter.WebAPI
             }
 
             app.UseHttpsRedirection();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "InterTest API V1");
+            });
 
             app.UseRouting();
 
