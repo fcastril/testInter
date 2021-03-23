bf503ed807de9db2c5bbedab378aa28767f48d21
Fabián Mauricio Castrillón Franco <fcastril@gmail.com>
2021-03-23 16:55:37 -0500

    tests

--- backend/testInter/testInter.test/UserControllerTests.cs
+++ backend/testInter/testInter.test/UserControllerTests.cs
@@ -7,6 +7,8 @@ using testInter.Repo;
 using testInter.WebAPI.Controllers;
 using System;
 using System.Net.Http;
+using Moq;
+using Microsoft.EntityFrameworkCore;
 
 namespace testInter.test
 {
@@ -15,12 +17,24 @@ namespace testInter.test
     [TestClass]
     public class UserControllerTests
     {
-        private UserService _userService;
+
+
         [TestMethod]
         public void GetUser_ReturnOK_AllRegisters()
         {
             #region Arrange
-            var userController = new UserController(_userService);
+            List<User> listUser = new List<User>();
+            User User = new User { Id = 1, Email = "login1@dominio.com", Password = "Abcd1234" };
+            listUser.Add(User);
+            User = new User { Id = 2, Email = "login2@dominio.com", Password = "Abcd1234" };
+            listUser.Add(User);
+            User = new User { Id = 3, Email = "login3@dominio.com", Password = "Abcd1234" };
+            listUser.Add(User);
+
+            var mockUsuarios = new Mock<IUserService>();
+            mockUsuarios.Setup(sp => sp.GetUsers()).Returns(listUser);
+
+            var userController = new UserController(mockUsuarios.Object);
             #endregion
 
 
@@ -31,9 +45,34 @@ namespace testInter.test
 
             #region Assert
             // The 'Response' is of type OkResult and returns an Object (OkObjectResult)..
-            Assert.IsInstanceOfType(response, typeof(OkObjectResult));
             //The Object returned by the OkResult is of type IEnumerable<Country>.
             Assert.IsInstanceOfType((response as OkObjectResult).Value, typeof(IEnumerable<User>));
+            #endregion
+        }
+        [TestMethod]
+        public void GetUser_ReturnOK()
+        {
+            #region Arrange
+
+            int id = 1;
+            User User = new User { Id = 1, Email = "login1@dominio.com", Password = "Abcd1234" };
+
+            var mockUsuarios = new Mock<IUserService>();
+            mockUsuarios.Setup(sp => sp.GetUser(id)).Returns(User);
+
+            var userController = new UserController(mockUsuarios.Object);
+            #endregion
+
+
+            #region Act
+            var response = userController.GetUser(id);
+
+            #endregion
+
+            #region Assert
+            // The 'Response' is of type OkResult and returns an Object (OkObjectResult)..
+            //The Object returned by the OkResult is of type IEnumerable<Country>.
+            Assert.IsInstanceOfType((response as OkObjectResult).Value, typeof(User));
             #endregion
         }
     }
